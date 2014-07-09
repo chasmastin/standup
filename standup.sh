a25ae98cc8c9b41635fd23576133b5bae03bdbf4
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-09 13:41:54 -0400

    De-crufting.

--- standup.sh
+++ standup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-cp com.cg.standup.plist /Library/LaunchDaemons/
+
+dir=$(pwd)
+cp ${dir}/com.cg.standup.plist /Library/LaunchDaemons/
 launchctl load /Library/LaunchDaemons/com.cg.standup.plist
 cd /tmp/ && curl -O http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
 osascript -e 'set Volume 6'
