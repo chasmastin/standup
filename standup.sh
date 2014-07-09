6b0bc110cf0f12b031f27039f760e9e6fb9fd19d
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-09 13:22:32 -0400

    Cruft removal.

--- standup.sh
+++ standup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-cp ./com.cg.standup.plist /Library/LaunchDaemons/
+cp com.cg.standup.plist /Library/LaunchDaemons/
 launchctl load /Library/LaunchDaemons/com.cg.standup.plist
-chown root:root /Library/LaunchDaemons/com.cg.standup.plist
 cd /tmp/ && curl -O http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
 osascript -e 'set Volume 6'
 afplay /tmp/Bob-Marley-Get-Up-Stand-Up.mp3
