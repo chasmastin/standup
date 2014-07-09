272f34223fed023fd7fa687580c402f12bc447cc
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-09 17:41:17 -0400

    Added marley ascii window popup.

--- marley.sh
+++ marley.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
-#  ╔═╗┌─┐┌┬┐  ┬ ┬┌─┐  ┌─┐┌┬┐┌─┐┌┐┌┌┬┐  ┬ ┬┌─┐┬
-#  ║ ╦├┤  │   │ │├─┘  └─┐ │ ├─┤│││ ││  │ │├─┘│
-#  ╚═╝└─┘ ┴   └─┘┴┘   └─┘ ┴ ┴ ┴┘└┘─┴┘  └─┘┴  o
-dir=$(pwd)
-cp ${dir}/com.cg.standup.plist /Library/LaunchDaemons/
-cp ${dir}/marley.sh /tmp/
-launchctl load /Library/LaunchDaemons/com.cg.standup.plist
-cd /tmp/ && curl -O http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
-osascript -e 'tell application "Terminal" to do script "/tmp/marley.sh"'
-osascript -e 'set Volume 0'
-afplay /tmp/Bob-Marley-Get-Up-Stand-Up.mp3
-#████████████▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████
+printf '\e[8;41;47t'
+echo "#████████████▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████
 #████████████▄▄▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░███
 #███████▀█▀███████▄▄░░░░░░░░░░░░░░░░░░░░░░░░░▀█
 #███████▀░░░░▄█████▄░░░░░░░░░░░░░░░░░░░░░░░░░░█
@@ -48,4 +38,4 @@ afplay /tmp/Bob-Marley-Get-Up-Stand-Up.mp3
 #██░░░░░░▄██▀░░░░▄▄▄▄▄▀▀▀░░░░░░░░░░░░░░░░░░████
 #█░░░░░▄███░░░░▄████░░░░░░░░░░░░░░░░░░░░░░░░███
 #░░░░░▄███▀░░░▄███▀░░░░░░░░░░▄░░░░░░░░░░░░░░░██
-#░░░░▄███░░░░███▀░░░░░░▄██████▀░░░░░░░░░░░░░░░█
+#░░░░▄███░░░░███▀░░░░░░▄██████▀░░░░░░░░░░░░░░░█"
