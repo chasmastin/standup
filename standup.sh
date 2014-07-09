46ffe5d7b1612762b852695dad28fff79d3a27b6
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-09 12:19:45 -0400

    Added syntax to move the plist into place and correct perms.

--- standup.sh
+++ standup.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+cp ./com.cg.standup.plist /Library/LaunchDaemons/
+launchctl load /Library/LaunchDaemons/com.cg.standup.plist
+chown root:root /Library/LaunchDaemons/com.cg.standup.plist
 cd /tmp/ && curl -O http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
 osascript -e 'set Volume 6'
 afplay /tmp/Bob-Marley-Get-Up-Stand-Up.mp3
