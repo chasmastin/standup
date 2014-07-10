85393b1d642bef412f405ace782941cc36f9d53b
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-10 11:27:22 -0400

    Fixing plist launch

--- standup.sh
+++ standup.sh
@@ -5,6 +5,7 @@
 dir=$(pwd)
 cp ${dir}/com.cg.standup.plist /Library/LaunchDaemons/
 cp ${dir}/marley.sh /tmp/
+cp ${dir}/standup.sh /tmp/
 launchctl load /Library/LaunchDaemons/com.cg.standup.plist
 cd /tmp/ && curl -O http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
 osascript -e 'tell application "Terminal" to do script "/tmp/marley.sh"'
