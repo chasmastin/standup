6ab9756d70ca28059ff4bc913da3ce8bbc48e609
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-11 09:35:35 -0400

    Set volume to a reasonable value.

--- standup.sh
+++ standup.sh
@@ -9,5 +9,5 @@ cp ${dir}/standup.sh /tmp/
 launchctl load /Library/LaunchDaemons/com.cg.standup.plist
 cd /tmp/ && curl -O http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
 osascript -e 'tell application "Terminal" to do script "/tmp/marley.sh"'
-osascript -e 'set Volume 6'
+osascript -e 'set Volume 3'
 afplay /tmp/Bob-Marley-Get-Up-Stand-Up.mp3
