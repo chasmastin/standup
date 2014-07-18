f11be845ec8bc9acb5fa5d0216d57e70c82acd2f
Brian Woestehoff <brian.woestehoff@controlgroup.com>
2014-07-18 11:43:04 -0400

    anon'ed the audio file name for easier variables

--- standup.sh
+++ standup.sh
@@ -6,8 +6,10 @@ dir=$(pwd)
 plist=/Library/LaunchDaemons/com.cg.standup.plist
 popup=/tmp/marley.sh
 thisscript=/tmp/standup.sh
-audiofile=/tmp/Eat_All_The_Old_People.mp3
-linktofile=https://dl.dropboxusercontent.com/u/59652646/Eat_All_The_Old_People.mp3
+audiofile=/tmp/standup.mp3
+#Uncomment the following line for the Cha$ rap version
+#linktofile=https://dl.dropboxusercontent.com/u/59652646/Eat_All_The_Old_People.mp3
+linktofile=http://www.angelxp.eu/sonnerie/b/m/Bob-Marley-Get-Up-Stand-Up.mp3
 if [ ! -f $plist ]; then
   cp ${dir}/com.cg.standup.plist /Library/LaunchDaemons/
 fi
@@ -23,7 +25,7 @@ if [ $? != 0 ]; then
     launchctl load /Library/LaunchDaemons/com.cg.standup.plist
 fi
 if [ ! -f $audiofile ]; then
-    cd /tmp/ && curl -O $linktofile
+    cd /tmp/ && curl $linktofile > $audiofile
 fi
 osascript -e 'tell application "Terminal" to do script "/tmp/marley.sh"'
 osascript -e 'set Volume 1'
